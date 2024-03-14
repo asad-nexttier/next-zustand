4f4bb0e919525483a3d48077732ef24bc9137fc4
Asad Mumtaz <asad.mumtaz@nexttier.io>
2024-03-14 16:03:11 +0500

    new code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
-import HomePage from "./home/page";
+import Link from "next/link";
 
-export default function Home() {
-    return <HomePage />
+import { getProducts } from "@/api/products";
+import ProductCard from "./components/ProductCard";
+
+export default async function Home() {
+  const products = await getProducts();
+  return (
+    <div className="flex flex-wrap gap-2">
+      <ul role="list" className="flex flex-row flex-wrap m-2">
+        {products.map((product) => (
+          <li key={product.id} className="md:w-1/3">
+            <Link href={`/products/${product.id}`}>
+              <ProductCard {...product} />
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
